e9531c7ff9a6faaa6839be94361fd1a714566ad0
Anjali2192 <967anjalimishra@gmail.com>
2026-08-01 00:44:50 +0530

    Saved all fields in an excel file

--- news combinator/scrape.py
+++ news combinator/scrape.py
@@ -1,7 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
-n = 0
+links = []
+titles = []
+authors = []
+scores = []
+comments = []
+
 headers = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36"
 }
@@ -13,11 +19,36 @@ if response.status_code == 200:
         link = i('a')[0].get('href')
         title = i.text.strip()
         author = url + i('a')[1].get('href')
-    for i in soup.find_all('span', 'score'):
-        score = i.text.strip().removesuffix(' points')
-    for span in soup.find_all('span', 'subline'):
-        a = span.find_all('a')
-        comments = a[3].text.strip()
+        links.append(link)
+        titles.append(title)
+        authors.append(author)
+
+    for i in soup.find_all('td', 'subtext'):
+        score_l = i.find('span', 'score')
+        if score_l is None:
+            scores.append('N/A')
+        else:
+            score = score_l.text.strip().removesuffix(' points')
+            scores.append(score)
+
+        comment_l = i.find('span', 'subline')
+        if comment_l is None:
+            comments.append('N/A')
+        else:
+            a = comment_l.find_all('a')
+            comment = a[3].text.strip()
+            comments.append(comment)
 
 else:
     print(f"Error {response.status_code}")
+
+df = pd.DataFrame({
+    "TITLE": titles,
+    "LINK": links,
+    "AUTHOR": authors,
+    "SCORE": scores,
+    "COMMENTS": comments
+})
+
+df.to_excel("news.xlsx", index=False)
+print("Excel file saved!")
