03397f596f1ff047ff74b2ff446978779d40a68b
Anjali2192 <967anjalimishra@gmail.com>
2026-08-05 22:30:52 +0530

    Parsed infobox

--- wikipedia/scrape.py
+++ wikipedia/scrape.py
@@ -13,6 +13,17 @@ if response.status_code == 200:
     last_modified_date = soup.find(id="footer-info-lastmod").text.strip().removeprefix("This page was last edited on ")
     p = soup.find_all('p')
     first_para = p[1].text.strip()
+
+    # infobox
+    infobox = soup.find('table', 'infobox vevent')
+    for t in infobox.find_all('tr'):
+        if t.find('th') is None or t.find('td') is None:
+            continue
+        else:
+            th = t.find('th').text.strip()
+            td = t.find('td').text.strip()
+            dic = dict(th=td)
+    print(dic)
    
 else:
     print(f"Error {response.status_code}")
