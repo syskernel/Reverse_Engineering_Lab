b503b1209611fd6bd22c4a0e4fc7b2a34de01e35
Anjali2192 <967anjalimishra@gmail.com>
2026-08-15 17:16:06 +0530

    Updated the script

--- python org/dashboard.py
+++ python org/dashboard.py
@@ -3,13 +3,14 @@ from python_org import org
 
 st.markdown("""
 <h1 style="
-    font-size: 50px;
-    margin-left: 60px;
+    font-size: 40px;
+    margin-left: 0px;
 ">
    Welcome to the scraper of python.org!
 </h1>
 """, unsafe_allow_html=True)
 
 version = st.text_input('Enter the python version which you would like to see!').replace('.', '')
-data = org(version)
-st.json(data)
+if version:
+    data = org(version)
+    st.json(data)
